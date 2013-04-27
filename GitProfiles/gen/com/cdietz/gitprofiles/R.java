e4c214438fc5683233b114b28c4b8816dc9112eb
Christopher Dietz <chris.m.dietz@gmail.com>
2013-04-26 20:18:54 -0500

    Minor UI enhancements

--- GitProfiles/gen/com/cdietz/gitprofiles/R.java
+++ GitProfiles/gen/com/cdietz/gitprofiles/R.java
@@ -18,17 +18,18 @@ public final class R {
         public static final int refresh=0x7f020004;
     }
     public static final class id {
-        public static final int events_list_fragment=0x7f060007;
-        public static final int main_profile_fragment=0x7f060006;
-        public static final int mainp_bio=0x7f060004;
+        public static final int events_list_fragment=0x7f060008;
+        public static final int main_profile_fragment=0x7f060007;
+        public static final int mainp_bio=0x7f060005;
+        public static final int mainp_bio_title=0x7f060004;
         public static final int mainp_html_url=0x7f060003;
         public static final int mainp_profile_pic=0x7f060000;
         public static final int mainp_real_name=0x7f060001;
         public static final int mainp_user_name=0x7f060002;
-        public static final int prompt_button=0x7f060009;
-        public static final int prompt_fragment=0x7f060005;
-        public static final int prompt_progress_circle=0x7f06000a;
-        public static final int prompt_text=0x7f060008;
+        public static final int prompt_button=0x7f06000a;
+        public static final int prompt_fragment=0x7f060006;
+        public static final int prompt_progress_circle=0x7f06000b;
+        public static final int prompt_text=0x7f060009;
     }
     public static final class layout {
         public static final int event_row=0x7f030000;
@@ -39,13 +40,20 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int blank=0x7f040001;
+        public static final int mainp_biography=0x7f040007;
         /**  main profile page strings 
          */
-        public static final int mainp_default=0x7f040002;
+        public static final int mainp_default=0x7f040003;
+        public static final int mainp_default_real_name=0x7f040004;
+        public static final int mainp_default_url=0x7f040006;
+        public static final int mainp_default_user_name=0x7f040005;
         /**  prompt string 
          */
-        public static final int prompt_enter_profile_hint=0x7f040003;
-        public static final int prompt_view_profile=0x7f040004;
+        public static final int prompt_enter_profile_hint=0x7f040008;
+        public static final int prompt_view_profile=0x7f040009;
+        /**  main page strings 
+         */
+        public static final int recent_activity=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
